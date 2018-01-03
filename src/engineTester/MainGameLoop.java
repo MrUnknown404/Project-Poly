a7a8998527ec4ecbe74354c66ba607b8c5657353
DESKTOP-Q2MP1SP\-Unknown- <torrinmartel@gmail.com>
2018-01-03 01:01:37 -0700

    3D Model Loader

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -22,92 +23,18 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 		
-		//float[] vertices = {-0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f};
-		//int[] indices = {0,1,3, 3,1,2};
-		//float[] textureCoords = {0,0, 0,1, 1,1, 1,0};
+		RawModel model = OBJLoader.loadObjModel("Stall", loader);
 		
-		float[] vertices = {
-				-0.5f,0.5f,-0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,-0.5f,0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,
-				-0.5f,-0.5f,-0.5f,
-				-0.5f,-0.5f,0.5f,
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f};
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("stallTexture")));
 		
-		float[] textureCoords = {
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0};
-		
-		int[] indices = {
-				0,1,3,
-				3,1,2,
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("DebugMap-256X256")));
-		
-		Entity entity = new Entity(staticModel, new Vector3f(0.0f, 0.0f, -3.5f), 0.0f, 0.0f, 0.0f, 1.0f); //X Y Z RX RY RZ Scale
+		Entity entity = new Entity(staticModel, new Vector3f(0.0f, -2.5f, -20f), 0.0f, 0.0f, 0.0f, 1.0f); //X Y Z RX RY RZ Scale
 		
 		Camera camera = new Camera();
 		
 		while (!Display.isCloseRequested()) {
 			//entity.increasePosition(0.0f, 0.0f, -0.02f); //XYZ
-			entity.increaseRotation(0.0f, 0.65f, 0.65f); //XYZ
+			//entity.increaseRotation(0.0f, 0.65f, 0.65f); //XYZ
+			entity.increaseRotation(0.0f, 0.65f, 0.0f); //XYZ
 			camera.move();
 			renderer.prepare();
 			shader.start();
